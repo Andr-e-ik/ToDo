04e6c14a8ae8a35bdfae2a949e279eec97595c9b
Andr-e-ik <147918351+Andr-e-ik@users.noreply.github.com>
2024-09-04 10:45:33 +0300

    Update database

--- db/database.py
+++ db/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-# "postgresql+asyncpg://postgres:postgres@localhost/my_felo"
-DATABASE_URL = "postgresql+asyncpg://newuser:newpassword@localhost/newdb"
 
-# engine = create_engine(DATABASE_URL)
+DATABASE_URL = ""
+
 engine = create_async_engine(DATABASE_URL, pool_recycle=1800, echo=False)
 
 
